0d91a5fe729610cd5cbebfa3e092ce1bb160834d
Yesim nur Akar <yesimnurakar@gmail.com>
2021-06-20 14:59:22 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,41 +3,30 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
   let form = document.getElementById("create-task-form")
-  form.addEventListener("submit", (e) => e.preventDefault())
+form.addEventListener("submit", (e) => e.preventDefault())
 
-  let tasklist = document.getElementById("tasks");
-  tasklist.style.padding = "3"
-  let input = document.getElementById("new-task-description");
-  let button= document.querySelector(".submit-btn");
-  let counterForId = 0;
+let ul = document.getElementById("tasks");
+ul.style.padding = "0"
+let input = document.getElementById("new-task-description");
+let btn = document.querySelector(".submit-btn");
+let counterForId = 0; 
 
-  const handleButtonClick = (e) => {
-    let no = document.getElementById(e.target.parentElement.id)
-    no.remove()
+const handleButtonClick = (e) => {
+  let x = document.getElementById(e.target.parentElement.id)
+  x.remove()
+}
+
+btn.addEventListener("click", () => {
+  if (input.value === "") { console.log("You didn't write anything!")}
+  else {
+    let li = document.createElement("li");
+    li.id = ++counterForId;
+    li.innerText = input.value;
+
+    let removeBtn = document.createElement("button");
+    removeBtn.innerText = "X";
+    removeBtn.addEventListener("click", handleButtonClick)
+    li.appendChild(removeBtn)
+    ul.appendChild(li);
   }
-
-  button.addEventListener("click", () => {
-    if (input.value === "") { console.log("You didn't write anything!")}
-    else {
-      let li = document.createElement("li");
-      li.id = ++counterForId;
-      li.innerText = input.value;
-
-      let removeBtn = document.createElement("button");
-      removeBtn.innerText = "X";
-      removeBtn.addEventListener("click", handleButtonClick)
-      li.appendChild(removeBtn)
-      tasklist.appendChild(li);
-    }
-  })
-const newtask = document.getElementById("new-task-description");
-const tasklist = document.getElementById("tasks");
-const div = document.getElementById("list");
-const button = document.getElementById("button");
-
-button.addEventListener('click', function (e){
-  let listitem = document.createElement('li');
-  listitem.innerText = newtask.value;
-  div.append(listitem);
-   event.preventDefault();
-});
+}) 
