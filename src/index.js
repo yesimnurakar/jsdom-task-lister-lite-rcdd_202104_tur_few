56b41c3f120de7b554ff21ae0789f04454bea589
Yesim nur Akar <yesimnurakar@gmail.com>
2021-06-20 15:13:09 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+let newtask=document.getElementById('new-task-description');
+let u=document.getElementById('tasks');
+
+
+let b =document.getElementById('button');
+let form=document.getElementById('create-task-form')
+form.addEventListener("submit", function(e){
+  e.preventDefault();
+
+let LI=document.createElement("li");
+let B=document.createElement("button")
+B.setAttribute("class","buttondone");
+LI.innerHTML+=newtask.value;
+B.innerHTML="Done";
+u.appendChild(LI);
+LI.appendChild(B);
+newtask.value="";
+console.log("DONE");
+B.addEventListener("click", function(e){
+  e.preventDefault();
+  LI.remove();
 });
+});});
