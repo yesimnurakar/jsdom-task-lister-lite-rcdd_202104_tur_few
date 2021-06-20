a54d33ed110852780589847da60d2db20502a405
Yesim nur Akar <yesimnurakar@gmail.com>
2021-06-20 15:00:12 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,32 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 
-
-  let form = document.getElementById("create-task-form")
-form.addEventListener("submit", (e) => e.preventDefault())
-
-let ul = document.getElementById("tasks");
-ul.style.padding = "0"
-let input = document.getElementById("new-task-description");
-let btn = document.querySelector(".submit-btn");
-let counterForId = 0;
-
-const handleButtonClick = (e) => {
-  let x = document.getElementById(e.target.parentElement.id)
-  x.remove()
-}
-
-btn.addEventListener("click", () => {
-  if (input.value === "") { console.log("You didn't write anything!")}
-  else {
-    let li = document.createElement("li");
-    li.id = ++counterForId;
-    li.innerText = input.value;
-
-    let removeBtn = document.createElement("button");
-    removeBtn.innerText = "X";
-    removeBtn.addEventListener("click", handleButtonClick)
-    li.appendChild(removeBtn)
-    ul.appendChild(li);
-  }
-}) 
+  let taskForm = document.getElementById("create-task-form");
+  
+    taskForm.addEventListener('submit', function (e){
+     e.preventDefault()
+     let inputTask = document.getElementById("new-task-description").value;
+  
+     let li = document.createElement("li");
+     let text = document.createTextNode(inputTask);
+  
+     li.appendChild(text);
+     tasks.appendChild(li);
+  
+    })
+  
+  });
