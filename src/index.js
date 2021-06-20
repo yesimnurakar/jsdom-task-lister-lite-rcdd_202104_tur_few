9c06a45c9e047c436111ca9d63aee4314f816b18
Yesim nur Akar <yesimnurakar@gmail.com>
2021-06-20 14:53:04 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+
+const newtask = document.getElementById("new-task-description");
+const ul = document.getElementById("tasks");
+const div = document.getElementById("list");
+const button = document.getElementById("button");
+
+button.addEventListener('click', function (e){
+  let listitem = document.createElement('li');
+  listitem.innerText = newtask.value;
+  div.append(listitem);
+   event.preventDefault();
 });
